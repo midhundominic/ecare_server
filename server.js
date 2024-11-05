6540c7b152c90f714109a502c9076074969ea547
midhundominic <111724929+midhundominic@users.noreply.github.com>
2024-11-06 01:26:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use("/api", authRoutes); // Use the routes
 // app.use('/api/appointments', appointmentRoutes);
 // app.use("/healthData",healthDataRoutes);
 
-// app.use('/src/assets/doctorProfile', express.static('src/assets/doctorProfile'));
-// app.use('/src/assets', express.static('src/assets'));
+app.use('/src/assets/doctorProfile', express.static('src/assets/doctorProfile'));
+app.use('/src/assets', express.static('src/assets'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
