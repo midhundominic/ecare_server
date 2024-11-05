019315291542727e3e99e53cbe67c73737299c23
midhundominic <111724929+midhundominic@users.noreply.github.com>
2024-11-05 23:56:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,20 +7,8 @@ require('dotenv').config();
 const app = express();
 
 // Enable CORS for all routes
-const allowedOrigins = [
-  'https://euphonious-sunshine-b6409e.netlify.app',
-  'https://peaceful-kataifi-11d5db.netlify.app'
-];
+app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 
@@ -31,8 +19,6 @@ app.use("/api", authRoutes); // Use the routes
 
 // app.use('/src/assets/doctorProfile', express.static('src/assets/doctorProfile'));
 // app.use('/src/assets', express.static('src/assets'));
-app.use('/assets', express.static('src/assets'));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -40,8 +26,8 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
